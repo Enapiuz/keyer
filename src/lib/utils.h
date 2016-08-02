cbba59bd820a9beb31ee4f8a24582d3185a5bb74
Enapiuz <enapiuz@gmail.com>
2016-08-03 01:18:16 +0300

    jenkins hash

---
+++ src/lib/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t jenkins_hash(char *key, size_t len);
--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+#include "lib/utils.h"
 
 int main(int argc, char *argv[]) {
-    puts("Hello!");
+    char *key = "megakey";
+    uint32_t hash = jenkins_hash(key, strlen(key));
+
+    printf("%d and original key length %zu", hash, strlen(key));
     return 0;
 }
