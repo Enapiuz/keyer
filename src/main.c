c64c84b2f07b3cbfc2979f8ea70c0dddc7d22e1b
Enapiuz <enapiuz@gmail.com>
2016-08-04 00:49:20 +0300

    glib, but "undefined reference to `g_str_equal'" error :(

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include "lib/utils.h"
+#include <glib.h>
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    char key[10];
-    snprintf(key, 10, "%d", rand());
-    uint32_t hash = jenkins_hash(key, strlen(key));
-
-    printf("Hash is %d and original key is \"%s\" with length %zu", hash, key, strlen(key));
+    GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+    g_hash_table_insert(hash, "Virginia", "Richmond");
+    g_hash_table_insert(hash, "Texas", "Austin");
+    g_hash_table_insert(hash, "Ohio", "Columbus");
+    printf("There are %d keys in the hash\n", g_hash_table_size(hash));
+    printf("The capital of Texas is %s\n", (char *)g_hash_table_lookup(hash, "Texas"));
+    gboolean found = g_hash_table_remove(hash, "Virginia");
+    printf("The value 'Virginia' was %sfound and removed\n", found ? "" : "not ");
+    g_hash_table_destroy(hash);
     return 0;
 }
